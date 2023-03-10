16c3c41f217d990da44bb21b63549c51067421ec
Eva291204 <eva.fricaud@e-artsup.net>
2023-03-10 13:48:37 +0100

    Début placement bâtiment

---
+++ Assets/Scripts/BuildPlacement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BuildPlacement : MonoBehaviour     //vidéo https://www.youtube.com/watch?v=ImiyFWZkMAA 
+{
+    public GameObject BuildToPlace;
+    public GameObject BuildToMove;
+    public LayerMask mask;
+    public float LastPosY;
+    public Vector3 MousePos;
+
+    private Renderer rend;
+    public Material matGrid, matDefault;
+
+    void Update()
+    {
+        MousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(MousePos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+        {
+            int Posx = (int)Mathf.Round(hit.point.x);
+            int Posz = (int)Mathf.Round(hit.point.z);
+
+            BuildToMove.transform.position = new Vector3(Posx, LastPosY, Posz);
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(BuildToPlace, BuildToMove.transform.position, Quaternion.identity);
+        }
+    }
+}
