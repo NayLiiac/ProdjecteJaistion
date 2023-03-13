6cc0d018c3adea4c37296d1b30501952dba3b627
Kaerouusse <kaeruketsu@gmail.com>
2023-03-13 15:38:10 +0100

    Start of progress bar UI element for Win Condition

---
+++ Assets/Scripts/WinCondition/WinConditionProgressUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinConditionProgressUI : MonoBehaviour
+{
+    [SerializeField] private int minimum;
+    [SerializeField] private int maximum;
+    private WinCondition winCondition;
+    public Image mask;
+
+    private void CurrentFill()
+    {
+        float currentOffset = winCondition.progressionWinCondition - minimum;
+        float maxOffset = maximum - minimum;
+        float fillAmount = currentOffset / maxOffset;
+        mask.fillAmount = fillAmount;
+    }
+
+
+
+
+
+
+}
