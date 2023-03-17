f65e3564a97992fb393a3f53a6d15ab01d8adaed
m v <morgane.voisin@e-artsup.net>
2023-03-17 10:49:29 +0100

    Placement des bâtiments

---
+++ Assets/SelectBuilding.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectBuilding : MonoBehaviour
+{
+    //BuildPlacement buildPlacement;
+
+    public void ChangeBuilding(GameObject newBuilding)
+    {
+        //buildPlacement.BuildToPlace = newBuilding;
+    }
+}
