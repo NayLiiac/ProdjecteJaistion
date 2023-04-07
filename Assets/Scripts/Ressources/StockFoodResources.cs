64a362e54573733f7e1df9a55874b6ed937ceacb
Eva291204 <eva.fricaud@e-artsup.net>
2023-04-07 12:45:37 +0200

    effet farm

--- Assets/Scripts/Ressources/StockFoodResources.cs
+++ Assets/Scripts/Ressources/StockFoodResources.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class StockFoodResources : MonoBehaviour
 {
     public GetFoodRessource GetFoodRessource;
+    public BuildPlacement build;
 
     public int FoodPickedUp = 0;
+    public int foodBoost = 2;
 
     //PickedUpResource and start coroutine for wait
     public void RecupResource()
     {
-        FoodPickedUp++;
+        if (build.isFarmBuilt == false)
+        {
+            FoodPickedUp++;
+        }
+        if (build.isFarmBuilt)
+        {
+            FoodPickedUp = FoodPickedUp + foodBoost;
+        }
         StartCoroutine(GetFoodRessource.WaitResource());
     }
 }
