1e39e0e4317d2f249a1e2a897dca85fb044e5635
Eva291204 <eva.fricaud@e-artsup.net>
2023-04-08 17:56:46 +0200

    UI + Anim horloge (a finir)

---
+++ Assets/Scripts/UI_Scripts/ClockTime.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockTime : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        /*
+        if()
+        {
+            animator.SetBool("TimePause", true);
+        }
+        */
+    }
+}
